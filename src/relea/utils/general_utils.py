1ee5cdb3f49377fbcc22647eeb6c0f70cbba93c7
Sameer Shrestha <arktetra@Sameers-MacBook-Pro.local>
2026-05-13 22:58:23 +0545

    add utils for getting trainable parameters

--- src/relea/utils/general_utils.py
+++ src/relea/utils/general_utils.py
@@ -1,6 +1,9 @@
 from collections.abc import Mapping
 from typing import Any, Sequence
 
+import numpy as np
+import random
+import torch
 import torch.nn as nn
 
 def has_instance(list: Sequence, type: Any):
@@ -8,7 +11,6 @@ def has_instance(list: Sequence, type: Any):
         if isinstance(o, type):
             return True
     return False
-
 
 def to_cpu(x):
     if isinstance(x, Mapping):
@@ -19,6 +21,15 @@ def to_cpu(x):
         return tuple(to_cpu(list(x)))
     return x.detach().cpu()
 
+def get_trainable_parameters(model: nn.Module):
+    n_params = 0
+
+    for param in model.parameters():
+        if param.requires_grad:
+            n_params += param.numel()
+
+    return n_params
+
 def get_total_parameters(model: nn.Module):
     n_params = 0
 
@@ -26,3 +37,16 @@ def get_total_parameters(model: nn.Module):
         n_params += param.numel()
 
     return n_params
+
+def apply_global_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
+    if torch.mps.is_available():
+        torch.mps.manual_seed(seed)
+        
