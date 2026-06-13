324fa343112fb9a6ab64b18350da90c860a460b1
ArkTetra <sameershrestha.sm@gmail.com>
2026-06-13 20:03:48 +0545

    minor changes

--- src/relea/callbacks/logging.py
+++ src/relea/callbacks/logging.py
@@ -34,28 +34,3 @@ class LoggingCallback(Callback):
 
     def after_train(self, trainer: "relea.Trainer"):
         self.logger.finish()
-
-class IterativeLoggingCallback(Callback):
-    order = 2
-
-    def __init__(self, logger):
-        super().__init__()
-        self.logger = logger 
-
-    def before_train(self, trainer: "relea.Trainer"):
-        self.losses = [k for k in trainer.__dict__.keys() if "loss" in k]
-
-    def after_batch(self, trainer: "relea.Trainer"):
-        self.logger.log(
-            {f"train/batch/{k}": trainer.__dict__[k] for k in self.losses}
-        )
-        if (trainer.step + 1) % trainer.eval_every != 0:
-            log_dict = {}
-
-            for k, m in trainer.metrics.all_metrics.items():
-                log_dict[k] = m.compute()
-
-            self.logger.log(log_dict)
-    
-    def after_train(self, trainer: "relea.Trainer"):
-        self.logger.finish()
