37d23e40bb77dafa0261eab11481dcc55f5ca45b
ArkTetra <sameershrestha.sm@gmail.com>
2026-06-13 20:00:42 +0545

    feat: extend checkpointing for iterative trainer

--- src/relea/callbacks/checkpoint.py
+++ src/relea/callbacks/checkpoint.py
@@ -7,36 +7,36 @@ from relea.callbacks import Callback
 
 
 class ModelCheckpoint(Callback):
-    def __init__(self, dir_path: Union[Path, str], track: str = "loss", every_epoch: bool = False, persist: bool = False):
+    def __init__(self, dir_path: Union[Path, str], track: str = "val_loss", every_eval: bool = False, persist: bool = False):
         super().__init__()
         self.track = track
         self.best_metric = float("inf") if "loss" in track else 0.
         self.best_metric_path = None
-        self.every_epoch = every_epoch
+        self.every_eval = every_eval
         self.persist = persist
 
         self.dir_path = Path(dir_path) if not isinstance(dir_path, Path) else dir_path
         self.dir_path.mkdir(parents=True, exist_ok=True)
-
-    def after_epoch(self, trainer: "relea.Trainer"):
+        
+    def _checkpoint(self, progress: str, trainer: "relea.Trainer"):
         if not trainer.enable_checkpointing:
             return
-        
-        metric = trainer.metrics.all_metrics[f"val_{self.track}"].compute()  # type: ignore
 
-        if self.every_epoch:
+        metric = trainer.metrics.all_metrics[self.track].compute()  # type: ignore
+
+        if self.every_eval:
             if self.persist:
-                filename = f"{type(trainer.model).__name__}-{trainer.epoch}-{self.track}-{metric:.4f}.pt"
+                filename = f"{type(trainer.model).__name__}-{progress}-{self.track}-{metric:.4f}.pt"
             else:
                 filename = f"{type(trainer.model).__name__}.pt"
             ckpt_path = self.dir_path / filename
 
             trainer.model.save_checkpoint(ckpt_path)
         else:
-            filename = f"{type(trainer.model).__name__}-{trainer.epoch}-{self.track}-{metric:.4f}.pt"
+            filename = f"{type(trainer.model).__name__}-{progress}-{self.track}-{metric:.4f}.pt"
             ckpt_path = self.dir_path / filename
 
-            if "loss" in self.track:
+            if "loss" in self.track or self.higher_is_better:
                 if metric < self.best_metric:
                     if self.best_metric_path is not None:
                         self.best_metric_path.unlink()
@@ -52,3 +52,14 @@ class ModelCheckpoint(Callback):
                     self.best_metric_path = ckpt_path
 
                     trainer.model.save_checkpoint(ckpt_path)
+        
+    def after_eval(self, trainer: "relea.Trainer"):
+        self._checkpoint(f"step-{trainer.step}", trainer)
+
+    def after_epoch(self, trainer: "relea.Trainer"):
+        self._checkpoint(f"epoch-{trainer.epoch}", trainer)
+
+    def before_train(self, trainer: "relea.Trainer"):
+        self.higher_is_better = trainer.metrics.all_metrics[self.track].higher_is_better
+        if not self.higher_is_better:
+            self.best_metric = float("inf")
