74c2bcf600e44e5fcb6726e8d1eb3ea262e31627
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2026-06-15 06:49:02 +0545

    Feat: implement safetensors

--- src/relea/models/base.py
+++ src/relea/models/base.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import Any
 from typing_extensions import override
+from safetensors.torch import save_file, load_file
+
 
 import torch
 import torch.nn as nn
@@ -22,7 +24,18 @@ class BaseModule(nn.Module):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
 
     def save_checkpoint(self, path: Path):
-        torch.save(self.state_dict(), path)
+        if(path.suffix == ".pt" or path.suffix == ".pth"): 
+            torch.save(self.state_dict(), path)
+        elif(path.suffix == ".safetensors"): 
+            save_file(self.state_dict(), str(path))
+        else:
+            raise ValueError(f"Unsupported checkpoint format: {path}")
 
     def load_checkpoint(self, path: Path):
-        self.load_state_dict(torch.load(path, map_location=self.device), strict=False)
+        if(path.suffix == ".pt" or path.suffix == ".pth"): 
+            self.load_state_dict(torch.load(path, map_location=self.device), strict=False)
+        elif(path.suffix == ".safetensors"): 
+            state_dict = load_file(str(path), device=self.device)
+            self.load_state_dict(state_dict, strict=False)
+        else:
+            raise ValueError(f"Unsupported checkpoint format: {path}")
