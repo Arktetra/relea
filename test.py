28641595f5f6a480a52f41a78e65d734127de0f1
abhi <abhijeetkc01@gmail.com>
2026-06-18 19:29:16 +0545

    test.py made more visualised

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from relea.data.miniimagenet import MiniImageNetDataModule
 import os
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     datamodule = MiniImageNetDataModule(root=os.getcwd())
@@ -9,6 +10,11 @@ if __name__ == "__main__":
     train_dataloader = datamodule.train_dataloader()
     batch = next(iter(train_dataloader))
     print(batch)
+    print(batch[0].dtype)
+    img = batch[0].squeeze().permute(1, 2, 0).numpy()
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
 
 # from datasets import load_dataset
 
