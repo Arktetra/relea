96a96c30b5da8f0479cb0dbe61fe5479bafba7d4
ArkTetra <sameershrestha.sm@gmail.com>
2026-06-19 12:44:43 +0545

    feat: add method to build model from config

--- src/relea/models/ucf.py
+++ src/relea/models/ucf.py
@@ -1,19 +1,30 @@
 from torchvision.models.video import mvit_v2_s, MViT_V2_S_Weights
 
 from relea.models.base import BaseModule
+from relea.utils.general_utils import get_trainable_parameters
 
 import torch.nn as nn
 import torch.nn.functional as F
 
 class MViTVideoClassifier(BaseModule):
-    def __init__(self, num_classes: int = 10, device: str = "cpu"):
+    def __init__(self, num_classes: int = 10):
         super().__init__()
         self.net = mvit_v2_s(weights=MViT_V2_S_Weights)
         in_features = self.net.head[1].in_features
         self.net.head[1] = nn.Linear(in_features=in_features, out_features=num_classes)
 
+        for name, param in self.net.named_parameters():
+            if "head" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
         self.loss = None
-        self.device = device
+
+    @staticmethod
+    def from_config(cfg: dict):
+        num_classes = cfg["num_classes"]
+        return MViTVideoClassifier(num_classes)
 
     def forward(self, X):
         self.net(X)
