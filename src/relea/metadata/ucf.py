088723f6487adc9d18b536f96dcd617c85681308
ArkTetra <sameershrestha.sm@gmail.com>
2026-06-19 09:39:56 +0545

    add metadata for UCF-subset

---
+++ src/relea/metadata/ucf.py
@@ -0,0 +1,14 @@
+IDX_TO_LABEL = {
+    0: "ApplyEyeMakeup",
+    1: "ApplyLipstick",
+    2: "Archery",
+    3: "BabyCrawling",
+    4: "BalanceBeam",
+    5: "BandMarching", 
+    6: "BaseballPitch",
+    7: "Basketball",
+    8: "BasketballDunk",
+    9: "BenchPress"
+}
+
+LABEL_TO_IDX = {v: k for k, v in IDX_TO_LABEL.items()}
