3701889e288460190d2417625b3f4c9ab2821e2b
ArkTetra <sameershrestha.sm@gmail.com>
2026-06-19 09:44:45 +0545

    fix: time issues in sampler

--- src/relea/samplers/euler.py
+++ src/relea/samplers/euler.py
@@ -7,11 +7,12 @@ class FlowEulerSampler(Sampler):
         super().__init__(model=flow_model)
 
     def sample(self, x, steps: int):
+        N, C, H, W = x.shape
         with torch.no_grad():
             h = 1 / steps
             xs = [x.clone()]
             for i in range(steps):
-                xs.append(self.model(x, torch.full((x.size(0), 1), i * h)))
+                xs.append(self.model(x, torch.full((N, C, H, W), i * h, device=x.device)))
                 x += xs[-1] * h
             
         return x, xs
